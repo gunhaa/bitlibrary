a76f47a17cda4ba4962394968874d54741eb649d
gunha <wh8299@gmail.com>
2025-01-06 15:23:16 +0900

    feat : update intro controller

--- src/main/java/bitcopark/library/controller/intro/IntroController.java
+++ src/main/java/bitcopark/library/controller/intro/IntroController.java
@@ -18,7 +18,7 @@ public class IntroController {
 
     private final CategoryRepository categoryRepository;
 
-//    @GetMapping("/intro/introduction")
+
     @GetMapping("/intro/{catLevel2}/{catLevel3}")
     public String intro(Model model , @ModelAttribute("categoryList") List<CategoryDTO> categoryDTOList
                         , @PathVariable String catLevel2, @PathVariable String catLevel3){
@@ -27,10 +27,10 @@ public class IntroController {
         CategoryDTO categoryLevel1 = getCategoryDTObyCategoryEngName(categoryDTOList, catLevel1);
         model.addAttribute("catLevel1", categoryLevel1.getId());
 
-
         CategoryDTO categoryLevel2 = getCategoryDTObyCategoryEngName(categoryDTOList, catLevel2);
         model.addAttribute("catLevel2", categoryLevel2.getId());
-
+        // Const enum 추가하기
+        // 
         if(categoryLevel2.getCategoryName().equals("도서관 소개")){
             CategoryDTO categoryLevel3 = getCategoryDTObyCategoryId(categoryDTOList, catLevel3);
             model.addAttribute("catLevel3", Integer.parseInt(catLevel3));
@@ -69,20 +69,10 @@ public class IntroController {
         }
 
         if(categoryLevel2.getCategoryName().equals("시설안내")) {
+
+            // dosomething
             return "intro/lib_map";
         }
-
-
-
-//            String category1 = "도서관 안내";
-//            String category2 = "도서관 소개";
-//            String category3 = "인사말";
-//
-//            List<Category> categories = categoryRepository.selectCategories(category1, category2, category3);
-//
-//            for(int i=1; i < categories.size()+1; i++){
-//            model.addAttribute("catLevel"+i, categories.get(i-1).getId());
-//            }
 
         return "common/main";
     }
