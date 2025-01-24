d885340074a96254b7b2dfcae55f4c93a97f1708
jw <jw140@naver.com>
2025-01-24 14:17:10 +0900

    rename: myLibrary -> study

--- src/main/java/bitcopark/library/controller/study/StudyController.java
+++ src/main/java/bitcopark/library/controller/study/StudyController.java
@@ -16,35 +16,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+import static bitcopark.library.controller.util.ControllerUtils.*;
+import static bitcopark.library.controller.util.ControllerUtils.setCategoryAndRoute;
+
 @Controller
 @RequiredArgsConstructor
 public class StudyController {
 
-    private final CategoryRepository categoryRepository;
 
     @GetMapping(value = {"/{catLevel1:study}/{catLevel2}/{catLevel3}", "/{catLevel1:study}/{catLevel2}/"})
     public String study(Model model, @ModelAttribute("categoryDTOList") List<CategoryDTO> categoryDTOList,
-                        @PathVariable(name = "catLevel1") String catLevel1,
-                        @PathVariable(name = "catLevel2") String catLevel2,
-                        @PathVariable(name = "catLevel3", required = false) String catLevel3,
+                        @PathVariable String catLevel1,
+                        @PathVariable String catLevel2,
+                        @PathVariable(required = false) String catLevel3,
                         @RequestParam(value = "sub", required = false) String subCategoryEngName) {
 
-        CategoryDTO categoryLevel1 = ControllerUtils.getCategoryByCategoryEngName(categoryDTOList, catLevel1);
-        model.addAttribute("catLevel1", categoryLevel1.getId());
+        CategoryRouterResult categoryRouterResult = setCategoryAndRoute(model, categoryDTOList, catLevel1, catLevel2, catLevel3);
 
-        CategoryDTO categoryLevel2 = ControllerUtils.getCategoryByCategoryEngName(categoryDTOList, catLevel2);
-        model.addAttribute("catLevel2", categoryLevel2.getId());
-
-        CategoryDTO categoryLevel3 = ControllerUtils.getCategoryByCategoryId(categoryDTOList, catLevel3);
-        if (categoryLevel3 != null) {
-            model.addAttribute("catLevel3", Integer.parseInt(catLevel3));
-        }
-
-        CategoryStrategy strategy = CategoryStrategyFactory.getStrategy(categoryLevel2);
-        CategoryRouter router = new CategoryRouter(strategy);
-
+        CategoryRouter router = categoryRouterResult.router();
+        CategoryDTO categoryLevel3 = categoryRouterResult.categoryLevel3();
         categoryLevel3.setSubCategoryEngName(subCategoryEngName);
 
         return router.routing(categoryLevel3);
     }
+
 }
