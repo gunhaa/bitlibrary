52cf44f53e1cf86f1cec9b288a0153ce9b69147c
배지환 <ppxcxz@gmail.com>
2025-02-05 19:30:13 +0900

    feat: 이미지 등록 수정

--- src/main/java/bitcopark/library/dto/ClassScheduleRequestDTO.java
+++ src/main/java/bitcopark/library/dto/ClassScheduleRequestDTO.java
@@ -1,14 +1,28 @@
 package bitcopark.library.dto;
 
+import bitcopark.library.entity.board.Board;
+import bitcopark.library.entity.clazz.ClassSchedule;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class ClassScheduleRequestDTO {
-    private LocalDate recruitmentStartDt;
-    private LocalDate recruitmentEndDt;
-    private LocalDate startDt;
-    private LocalDate endDt;
+    private LocalDateTime recruitmentStartDt;
+    private LocalDateTime recruitmentEndDt;
+    private LocalDateTime startDt;
+    private LocalDateTime endDt;
     private Long maxParticipant;
+
+    public ClassSchedule toEntity(Board board) {
+        return ClassSchedule.builder()
+                .recruitmentStartDate(recruitmentStartDt)
+                .recruitmentEndDate(recruitmentEndDt)
+                .startDate(startDt)
+                .endDate(endDt)
+                .maxParticipants(maxParticipant)
+                .board(board)
+                .build();
+    }
 }
