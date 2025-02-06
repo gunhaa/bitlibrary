2c98992f11dc50ab768c70ab592ec1dc6d1f809c
gunhaa <wh8299@gmail.com>
2025-02-06 09:31:28 +0900

    fix : introstrategy bug fix

--- src/main/java/bitcopark/library/categoryStrategy/IntroStrategy.java
+++ src/main/java/bitcopark/library/categoryStrategy/IntroStrategy.java
@@ -7,7 +7,7 @@ public class IntroStrategy implements CategoryStrategy{
     @Override
     public String routing(CategoryDTO categoryLevel3) {
         return switch(categoryLevel3.getCategoryName()){
-            case "인사말" -> "/intro/lib_greeting";
+            case "인사말" -> "intro/lib_greeting";
             case "연혁" -> "intro/lib_history";
             case "조직도" -> "intro/lib_organization";
             case "도서관 오시는 길" -> "intro/lib_intro";
