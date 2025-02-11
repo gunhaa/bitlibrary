1d99cfbc8737221136fb26a88a0afc261f5ce7b2
gunha <wh8299@gmail.com>
2025-02-11 16:19:10 +0900

    feat : bookRequestUpdate logic update, updateBookRequest Test add

--- src/test/java/bitcopark/library/service/bookReqeustPageTest/UpdateBookRequestTest.java
+++ src/test/java/bitcopark/library/service/bookReqeustPageTest/UpdateBookRequestTest.java
@@ -3,6 +3,7 @@ package bitcopark.library.service.bookReqeustPageTest;
 import bitcopark.library.controller.book.BookRequestCondition;
 import bitcopark.library.entity.member.BookRequest;
 import bitcopark.library.entity.member.Member;
+import bitcopark.library.jwt.LoginMemberDTO;
 import bitcopark.library.repository.book.BookRequestRepository;
 import bitcopark.library.service.Book.BookRequestService;
 import bitcopark.library.service.Member.MemberService;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -36,7 +39,8 @@ public class UpdateBookRequestTest {
         // 회원 가입
         String naverEmail = "wh8299@naver.com";
         String naverName = "naver YxUVriKN_IuaBzIWFfCBzzfnVc6SHEkDJtxV9fY8pxQ 황건하";
-        Member OAuthNaverGunha = memberService.joinOAuth2Member(naverEmail, naverName, "ROLE_ADMIN");
+        String role = "ROLE_USER";
+        Member OAuthNaverGunha = memberService.joinOAuth2Member(naverEmail, naverName, role);
 
         String isbn = "9788971992258";
 
@@ -50,14 +54,23 @@ public class UpdateBookRequestTest {
         bookRequestCondition.setBookPublicationDate(LocalDateTime.now().toLocalDate());
         bookRequestCondition.setOpinion("작성자 의견");
         bookRequestService.registerBookRequest(bookRequestCondition);
+        
 
         // when
         BookRequest bookRequest = bookRequestRepository.findByIsbn(isbn).get();
-        bookRequestService.updateBookRequest()
+
+        String updateOpinion = "업데이트된 작성자 의견";
+        String prevIsbn = "9788971992258";
+        bookRequestCondition.setOpinion(updateOpinion);
+        bookRequestCondition.setPrevIsbn(prevIsbn);
+        LoginMemberDTO loginMemberDTO = new LoginMemberDTO(naverEmail, naverName, role);
+        ResponseEntity<?> responseEntity = bookRequestService.updateBookRequest(bookRequestCondition, loginMemberDTO);
+        BookRequest updateBookRequest = bookRequestRepository.findByIsbn(isbn).get();
 
         //then
         Assertions.assertThat(bookRequest.getIsbn()).isEqualTo(isbn);
-
+        Assertions.assertThat(updateBookRequest.getOpinion()).isEqualTo(updateOpinion);
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
 }
