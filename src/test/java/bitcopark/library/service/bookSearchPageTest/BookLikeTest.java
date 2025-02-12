ddea53ae3f2e65924ea6ff631bf8daf0f56a150d
gunha <wh8299@gmail.com>
2025-02-12 15:36:05 +0900

    test : add bookliketoggle test

--- src/test/java/bitcopark/library/service/bookSearchPageTest/BookLikeTest.java
+++ src/test/java/bitcopark/library/service/bookSearchPageTest/BookLikeTest.java
@@ -1,14 +1,15 @@
 package bitcopark.library.service.bookSearchPageTest;
 
+import bitcopark.library.controller.search.LikeCondition;
+import bitcopark.library.controller.search.LikeStatus;
 import bitcopark.library.entity.book.Book;
+import bitcopark.library.entity.book.BookLike;
 import bitcopark.library.entity.book.BookState;
 import bitcopark.library.entity.book.BookSupple;
 import bitcopark.library.entity.member.Member;
 import bitcopark.library.jwt.LoginMemberDTO;
-import bitcopark.library.repository.book.BookLikeDto;
-import bitcopark.library.repository.book.BookSearchCondition;
-import bitcopark.library.repository.book.BookSearchDto;
-import bitcopark.library.repository.book.SearchType;
+import bitcopark.library.repository.book.*;
+import bitcopark.library.repository.member.MemberRepository;
 import bitcopark.library.service.Book.BookLikeService;
 import bitcopark.library.service.Book.BookService;
 import bitcopark.library.service.Book.SearchBooklistAndLikelistDTO;
@@ -21,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -35,6 +37,12 @@ public class BookLikeTest {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private BookLikeRepository bookLikeRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
     @DisplayName("좋아요")
     public void 좋아요() {
@@ -45,18 +53,12 @@ public class BookLikeTest {
         String naverName = "naver YxUVriKN_IuaBzIWFfCBzzfnVc6SHEkDJtxV9fY8pxQ 황건하";
         String role = "ROLE_USER";
         Member member1 = memberService.joinOAuth2Member(naverEmail, naverName, role);
-        Member member2 = memberService.joinOAuth2Member("test1@email.com", "naver YxUVriKN_IuaBzIWFfCBzzfnVc6SHEkDJtxV9fY8pxQ 황건하1", "ROLE_USER");
-        Member member3 = memberService.joinOAuth2Member("test2@email.com", "naver YxUVriKN_IuaBzIWFfCBzzfnVc6SHEkDJtxV9fY8pxQ 황건하2", "ROLE_USER");
-        Member member4 = memberService.joinOAuth2Member("test3@email.com", "naver YxUVriKN_IuaBzIWFfCBzzfnVc6SHEkDJtxV9fY8pxQ 황건하3", "ROLE_USER");
 
         // 책 등록
         Book 백범일지 = bookService.registerNewBook("김구 저", "백범일지", "돌베개", "2008", "9788971992258", "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F994701%3Ftimestamp%3D20240727112932", BookState.P, BookSupple.N);
 
         // 좋아요 등록
         bookLikeService.addBookLike(member1, 백범일지);
-        bookLikeService.addBookLike(member2, 백범일지);
-        bookLikeService.addBookLike(member3, 백범일지);
-        bookLikeService.addBookLike(member4, 백범일지);
 
         //when
         BookSearchCondition bookSearchCondition = new BookSearchCondition();
@@ -70,6 +72,44 @@ public class BookLikeTest {
         List<BookLikeDto> likeList = searchBooklistAndLikelistDTO.getLikeList();
         Assertions.assertThat(bookList.get(0).getIsbn()).isEqualTo(likeList.get(0).getIsbn());
 
+    }
+    
+    @Test
+    @DisplayName("좋아요_토글")
+    public void 좋아요_토글(){
+        //given
+
+        // 회원 가입
+        String naverEmail = "wh8299@naver.com";
+        String naverName = "naver YxUVriKN_IuaBzIWFfCBzzfnVc6SHEkDJtxV9fY8pxQ 황건하";
+        String role = "ROLE_USER";
+        Member member1 = memberService.joinOAuth2Member(naverEmail, naverName, role);
+
+        // 책 등록
+        String isbn = "9788971992258";
+        Book 백범일지 = bookService.registerNewBook("김구 저", "백범일지", "돌베개", "2008", isbn, "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F994701%3Ftimestamp%3D20240727112932", BookState.P, BookSupple.N);
+
+        // 좋아요 등록
+        bookLikeService.addBookLike(member1, 백범일지);
+
+        // when
+        LikeCondition likeCondition = new LikeCondition();
+        likeCondition.setIsbn(isbn);
+        likeCondition.setEmail(naverEmail);
+        likeCondition.setLikeStatus(LikeStatus.LIKED);
+        bookLikeService.toggleLike(likeCondition);
+        // then
+        List<BookLike> bookLikeList = bookLikeRepository.findByMember(member1);
+        Assertions.assertThat(bookLikeList.size()).isEqualTo(0);
+
+        //when,then
+        LikeCondition likeCondition2 = new LikeCondition();
+        likeCondition2.setIsbn(isbn);
+        likeCondition2.setEmail(naverEmail);
+        likeCondition2.setLikeStatus(LikeStatus.NOT_LIKED);
+        bookLikeService.toggleLike(likeCondition2);
+        List<BookLike> bookLikeList2 = bookLikeRepository.findByMember(member1);
+        Assertions.assertThat(bookLikeList2.size()).isEqualTo(1);
     }
 
 }
