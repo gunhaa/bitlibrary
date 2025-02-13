7c399b490c85df51cee51dcfa0e6f5da39083b3c
gunha <wh8299@gmail.com>
2025-02-13 13:20:58 +0900

    cors handler add

--- src/main/java/bitcopark/library/config/WebConfig.java
+++ src/main/java/bitcopark/library/config/WebConfig.java
@@ -1,6 +1,7 @@
 package bitcopark.library.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/board/**")
                 .addResourceLocations("file:/Users/baejihwan/uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("bitlibrary.kro.kr")  // 프로토콜 없이 도메인만 입력
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드 설정
+                .allowedHeaders("Authorization", "Content-Type")  // 허용할 헤더
+                .allowCredentials(true);  // 쿠키와 인증 정보 전송 허용
     }
 }
